cb57163a9fda79942b3e98a95560da62dfe61631
Alex Nemeth <alex.nemeth329@gmail.com>
2026-02-10 18:53:15 +0100

    Remove placeholder widget test

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('placeholder smoke test', (tester) async {
-    await tester.pumpWidget(const SizedBox.shrink());
-    expect(find.byType(SizedBox), findsOneWidget);
-  });
-}
